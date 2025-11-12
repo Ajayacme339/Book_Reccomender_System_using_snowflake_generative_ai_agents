2c3ab2b169b6c4978ea17a8534adf7928727fb4b
Ajay Kumar <60890279+Ajayacme339@users.noreply.github.com>
2025-11-12 01:49:30 -0500

    Create database and schema for books
    Verified if all the  records are available in new table

--- setup.sql
+++ setup.sql
@@ -1,7 +1,10 @@
-
+-----------------------------------------
 CREATE DATABASE BOOKS;
 CREATE SCHEMA BOOK_SCHEMA;
-
+----------------------------------------------
 CREATE TABLE BOOKS.BOOK_SCHEMA.GOODREADSBOOK
 AS 
 SELECT * FROM GOODREADS_BOOKS.PUBLIC.GOODREADSBOOK;
+-----------------------------------------------------
+SELECT COUNT(*) FROM GOODREADS_BOOKS.PUBLIC.GOODREADSBOOK;
+
